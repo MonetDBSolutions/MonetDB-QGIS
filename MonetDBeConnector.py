a05ffbe4c29f515954e51a6ec8bfbc55ae933de4
MitchellWeg <mitchell.w@live.nl>
2021-10-04 15:24:33 +0200

    show schema name and ID

--- MonetDBeConnector.py
+++ MonetDBeConnector.py
@@ -233,19 +233,29 @@ class MonetDBeConnector:
 
             self.db = monetdbeconn.MonetDB(username, password, hostname, database)
             table_conf = MonetDBeTableConfigDialog()
-            tables = self.db.query("SELECT name FROM sys.tables")
-            tables = [x[0] for x in tables]
+            tables = self.db.query("SELECT name, schema_id FROM sys._tables WHERE system = False")
+            _tables = [(x[0], x[1]) for x in tables]
+            tables = []
+            for i in _tables:
+                schema_names = self.db.query("SELECT name FROM sys.schemas WHERE id = " + str(i[1]))
+                i = (i[0], i[1], schema_names[0][0])
+                tables.append(i)
 
             table_conf.tableWidget.setRowCount(len(tables))
-            table_conf.tableWidget.setColumnCount(1)
+            table_conf.tableWidget.setColumnCount(3)
 
-            table_conf.tableWidget.setHorizontalHeaderLabels([u'Column1',
-                                                              u'Column2'])
+            table_conf.tableWidget.setHorizontalHeaderLabels([u'Table Name',
+                                                              u'Schema Name',
+                                                              u'Schema ID'])
 
             for row in range(len(tables)):
                 for col in range(1):
-                    item = QTableWidgetItem(str(tables[row]).strip())
-                    table_conf.tableWidget.setItem(row, 0, item)
+                    table_item = QTableWidgetItem(str(tables[row][0]).strip())
+                    table_conf.tableWidget.setItem(row, 0, table_item)
+                    table_schema_id = QTableWidgetItem(str(tables[row][1]).strip())
+                    table_conf.tableWidget.setItem(row, 2, table_schema_id)
+                    table_schema_name = QTableWidgetItem(str(tables[row][2]).strip())
+                    table_conf.tableWidget.setItem(row, 1, table_schema_name)
 
             table_conf.tableWidget.resizeColumnsToContents()
             table_conf.show()
@@ -269,6 +279,7 @@ class MonetDBeConnector:
 
         for i in data_points:
             data = i[0].strip()
+            print(data)
             fet.setGeometry(QgsGeometry.fromWkt(data))
             pr.addFeatures([fet])
 
